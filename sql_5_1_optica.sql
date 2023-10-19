c1485e4c3133efc3a3aa0cc08266324a2e8aae30
avila-ca <118176131+avila-ca@users.noreply.github.com>
2023-10-19 11:30:05 +0200

    updated files

--- sql_5_1_optica.sql
+++ sql_5_1_optica.sql
@@ -1,34 +1,34 @@
 CREATE TABLE IF NOT EXISTS `mydb`.`PROVEIDOR` (
+  `id_proveidor` INT NOT NULL,
   `NIF` VARCHAR(9) NOT NULL,
-  `nom` VARCHAR(45) NULL,
+  `nom` VARCHAR(45) NOT NULL,
   `adreça` VARCHAR(45) NULL,
   `telefon` INT NULL,
   `fax` INT NULL,
-  `id_proveidor` INT NOT NULL,
   PRIMARY KEY (`id_proveidor`))
 ENGINE = InnoDB
 CREATE TABLE IF NOT EXISTS `mydb`.`ULLERES` (
-  `marca` VARCHAR(30) NOT NULL,
-  `graduacio` DECIMAL(2) NULL,
-  `muntura` VARCHAR(45) NULL,
-  `colorMuntura` VARCHAR(45) NULL,
-  `colorVidre` VARCHAR(45) NULL,
-  `preu` DECIMAL(2) NULL,
   `id_ullera` INT NOT NULL,
-  `id_proveidor` INT NOT NULL,
+  `marca` VARCHAR(30) NOT NULL,
+  `graduacio` DECIMAL(2) NOT NULL,
+  `muntura` VARCHAR(45) NOT NULL,
+  `colorMuntura` VARCHAR(45) NOT NULL,
+  `colorVidre` VARCHAR(45) NOT NULL,
+  `preu` DECIMAL(2) NOT NULL,
   `EMPLEAT_ID_Client` INT NOT NULL,
   `EMPLEAT_id_empleat` INT NOT NULL,
-  PRIMARY KEY (`id_ullera`, `id_proveidor`, `EMPLEAT_ID_Client`, `EMPLEAT_id_empleat`),
-  INDEX `fk_ULLERES_PROVEIDOR1_idx` (`id_proveidor` ASC) VISIBLE,
+  `PROVEIDOR_id_proveidor` INT NOT NULL,
+  PRIMARY KEY (`EMPLEAT_ID_Client`, `EMPLEAT_id_empleat`, `id_ullera`, `PROVEIDOR_id_proveidor`),
   INDEX `fk_ULLERES_EMPLEAT1_idx` (`EMPLEAT_ID_Client` ASC, `EMPLEAT_id_empleat` ASC) VISIBLE,
-  CONSTRAINT `fk_ULLERES_PROVEIDOR1`
-    FOREIGN KEY (`id_proveidor`)
-    REFERENCES `mydb`.`PROVEIDOR` (`id_proveidor`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+  INDEX `fk_ULLERES_PROVEIDOR1_idx` (`PROVEIDOR_id_proveidor` ASC) VISIBLE,
   CONSTRAINT `fk_ULLERES_EMPLEAT1`
     FOREIGN KEY (`EMPLEAT_ID_Client` , `EMPLEAT_id_empleat`)
     REFERENCES `mydb`.`EMPLEAT` (`ID_Client` , `id_empleat`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_ULLERES_PROVEIDOR1`
+    FOREIGN KEY (`PROVEIDOR_id_proveidor`)
+    REFERENCES `mydb`.`PROVEIDOR` (`id_proveidor`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
@@ -52,9 +52,18 @@ CREATE TABLE IF NOT EXISTS `mydb`.`EMPLEAT_has_CLIENT` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_EMPLEAT_has_CLIENT_CLIENT1`
-    FOREIGN KEY (`CLIENT_ID_Client` , `CLIENT_recomana_IDClient`)
-    REFERENCES `mydb`.`CLIENT` (`ID_Client` , `recomana_IDClient`)
+    FOREIGN KEY (`CLIENT_recomana_IDClient`)
+    REFERENCES `mydb`.`CLIENT` (`recomana_IDClient`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
-
+CREATE TABLE IF NOT EXISTS `mydb`.`CLIENT` (
+  `id_client` INT NOT NULL,
+  `nom` VARCHAR(45) NOT NULL,
+  `adreça` VARCHAR(45) NULL,
+  `telefon` INT NULL,
+  `correu` VARCHAR(45) NULL,
+  `regData` DATE NULL,
+  `recomana_IDClient` INT NOT NULL,
+  PRIMARY KEY (`recomana_IDClient`, `id_client`))
+ENGINE = InnoDB
